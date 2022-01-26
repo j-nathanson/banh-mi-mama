a8f4d29febf75dc883ae7d8d12ddda0c0d67fddb
jacobnathanson <jacobnath93@gmail.com>
2022-01-26 16:46:22 -0500

    changed datato all menu items

---
+++ shared/menu.js
@@ -0,0 +1,92 @@
+export const Menu = [
+    {
+        id: 0,
+        name: "bánh mì thịt",
+        type: "banh mi"
+    },
+    {
+        id: 1,
+        name: "Bánh mì xíu mại",
+        type: "banh mi"
+    },
+    {
+        id: 2,
+        name: "Bánh mì chay",
+        type: "banh mi"
+    },
+    {
+        id: 3,
+        name: "Bánh mì heo quay",
+        type: "banh mi"
+    },
+    {
+        id: 4,
+        name: "Bánh mì ốp la",
+        type: "banh mi"
+    },
+    {
+        id: 5,
+        name: "Xôi gà",
+        type: "rice"
+    },
+    {
+        id: 6,
+        name: "Cơm tấm",
+        type: "rice"
+    },
+    {
+        id: 7,
+        name: "Cơm chiên",
+        type: "rice"
+    },
+    {
+        id: 8,
+        name: "Thịt Kho",
+        type: "rice"
+    },
+    {
+        id: 9,
+        name: "gỏi cuốn",
+        type: "sides"
+    },
+    {
+        id: 10,
+        name: "gỏi cuốn chay",
+        type: "sides"
+    },
+    {
+        id: 11,
+        name: "Bánh xèo",
+        type: "sides"
+    },
+    {
+        id: 12,
+        name: "Chả Giò ",
+        type: "sides"
+    },
+    {
+        id: 13,
+        name: "Nước Mía",
+        type: "drinks"
+    },
+    {
+        id: 14,
+        name: "Cà Phê Sữa đá",
+        type: "drinks"
+    },
+    {
+        id: 15,
+        name: "Cà Phê Đen Đá",
+        type: "drinks"
+    },
+    {
+        id: 16,
+        name: "Cà phê trứng",
+        type: "drinks"
+    },
+    {
+        id: 17,
+        name: "dừa tắc",
+        type: "drinks"
+    },
+]
