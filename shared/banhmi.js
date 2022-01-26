61df0bcb71e735bed94c66b84ba5a5ff54a83f18
jacobnathanson <jacobnath93@gmail.com>
2022-01-26 16:37:00 -0500

    banh mi data

---
+++ shared/banhmi.js
@@ -0,0 +1,22 @@
+export const BANH_MI_ITEMS = [
+    {
+        id: 0,
+        name: "bánh mì thịt"
+    },
+    {
+        id: 1,
+        name: "Bánh mì xíu mại"
+    },
+    {
+        id: 2,
+        name: "Bánh mì chay"
+    },
+    {
+        id: 3,
+        name: "Bánh mì heo quay"
+    },
+    {
+        id: 4,
+        name: "Bánh mì ốp la"
+    },
+]
