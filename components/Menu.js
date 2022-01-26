34546138f1f5a64777e9e47f96b255a0163288f8
jacobnathanson <jacobnath93@gmail.com>
2022-01-26 17:08:12 -0500

    tested data into a FlatList and ListItem components

--- components/Menu.js
+++ components/Menu.js
@@ -4,9 +4,24 @@ import { Button, View, Text, FlatList } from 'react-native';
 import { MENU } from "../shared/menuData";
 
 export default function MenuScreen({ navigation }) {
+
+    const renderMenuItem = ({ item }) => {
+        return (
+            <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quidem eius at quod </ListItem.Subtitle>
+            </ListItem.Content>
+        )
+    }
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Menu Screen</Text>
+            <FlatList
+                keyExtractor={item => item.id.toString()}
+                data={MENU}
+                renderItem={renderMenuItem}
+            />
             <Button
                 title="Go to menu item"
                 onPress={() =>
