5bbe1ab04c88ca380e1d7cac73d079a99472967c
jacobnathanson <jacobnath93@gmail.com>
2022-01-26 12:21:22 -0500

    stack test 2

--- App.js
+++ App.js
@@ -11,12 +11,21 @@ function HomeScreen() {
   );
 }
 
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
