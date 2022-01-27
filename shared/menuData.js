d759221acbe4fcbc039d271eb9ffcb81e4a1fbcc
jacobnathanson <jacobnath93@gmail.com>
2022-01-27 17:09:16 -0500

    added english titles to menu items

--- shared/menuData.js
+++ shared/menuData.js
@@ -1,28 +1,28 @@
 export const MENU = [
     {
         id: 0,
-        name: 'Bánh mì thịt',
+        name: 'Bánh mì thịt - (Classic) ',
         type: 'banh mi',
         image: require('../assets/images/banh-mi-thit.jpeg'),
         description: 'Assorted Vietnamese deli meats with cilantro, pickled daikon and carrots, chicken pâté, cilantro, and mayonnaise.'
     },
     {
         id: 1,
-        name: 'Bánh mì xíu mại',
+        name: 'Bánh mì xíu mại - (Meatballs)',
         type: 'banh mi',
         image: require('../assets/images/banh-mi-xiu-mai.jpg'),
         description: 'Pork meatballs with cilantro, pickled daikon and carrots, chicken pâté, cilantro, and mayonnaise.'
     },
     {
         id: 2,
-        name: 'Bánh mì chay',
+        name: 'Bánh mì chay - (Vegetarian)',
         type: 'banh mi',
         image: require('../assets/images/banh-mi-chay.jpg'),
         description: 'Seasoned tofu  with cilantro, pickled daikon and carrots, veggie pâté, cilantro, and mayonnaise.'
     },
     {
         id: 3,
-        name: 'Bánh mì heo quay',
+        name: 'Bánh mì heo quay - (Roast pork)',
         type: 'banh mi',
         image: require('../assets/images/banh-mi-heo-quay.jpg'),
         description: 'Roast pork with cilantro, pickled daikon and carrots, chicken pâté, cilantro, and mayonnaise.'
@@ -30,98 +30,98 @@ export const MENU = [
     },
     {
         id: 4,
-        name: 'Bánh mì ốp la',
+        name: 'Bánh mì ốp la - (Fried egg)',
         type: 'banh mi',
         image: require('../assets/images/banh-mi-op-la.jpeg'),
         description: 'Fried eggs with cilantro, pickled daikon and carrots, chicken pâté, cilantro, and mayonnaise.'
     },
     {
         id: 5,
-        name: 'Xôi gà',
+        name: 'Xôi gà - (Sticky rice)',
         type: 'rice',
         image: require('../assets/images/xoi-ga.jpg'),
         description: 'Sticky rice with chicken and fried shallots.'
     },
     {
         id: 6,
-        name: 'Cơm tấm',
+        name: 'Cơm tấm - (Broken rice)',
         type: 'rice',
         image: require('../assets/images/com-tam.jpg'),
         description: '"Broken rice" with a sweet marinaded pork chop, fresh veggies, and dipping sauce.'
     },
     {
         id: 7,
-        name: 'Cơm chiên',
+        name: 'Cơm chiên - (Fried Rice)',
         type: 'rice',
         image: require('../assets/images/com-chien.jpg'),
         description: 'Fried Vietnamese rice with mixed vegetables and egg'
     },
     {
         id: 8,
-        name: 'Thịt Kho',
+        name: 'Thịt Kho - (Braised pork & eggs)',
         type: 'rice',
         image: require('../assets/images/thit-kho.jpg'),
         description: 'White rice with marinated pork and boiled eggs braised in coconut juice'
     },
     {
         id: 9,
-        name: 'Gỏi cuốn',
+        name: 'Gỏi cuốn - (Shrimp spring rolls)',
         type: 'sides',
         image: require('../assets/images/goi-cuon.jpg'),
         description: 'Spring roll consisting of pork, shrimp and mixed vegetables with a peanut dipping sauce.'
     },
     {
         id: 10,
-        name: 'gỏi cuốn chay',
+        name: 'gỏi cuốn chay - (Tofu spring rolls)',
         type: 'sides',
         image: require('../assets/images/goi-cuon-chay.jpg'),
         description: 'Vegetarian Spring roll consisting of tofu and mixed vegetables with a peanut dipping sauce.'
     },
     {
         id: 11,
-        name: 'Bánh xèo',
+        name: 'Bánh xèo - (Crispy pancake)',
         type: 'sides',
         image: require('../assets/images/banh-xeo.jpg'),
         description: 'Crispy rice flour and turmeric pancake filled with shrimp, bean sprouts, and onions. '
     },
     {
         id: 12,
-        name: 'Chả Giò',
+        name: 'Chả Giò - (Fried spring rolls)',
         type: 'sides',
         image: require('../assets/images/cha-gio.jpg'),
         description: 'Fried spring rolls with ground pork and vegetables'
     },
     {
         id: 13,
-        name: 'Nước Mía',
+        name: 'Nước Mía - (Sugarcane juice)',
         type: 'drinks',
         image: require('../assets/images/nuoc-mia.jpg'),
         description: 'Iced Sugarcane juice with kumquats'
     },
     {
         id: 14,
-        name: 'Cà Phê Sữa đá',
+        name: 'Cà Phê Sữa - (Milk coffee)',
         type: 'drinks',
         image: require('../assets/images/ca-phe-sua.jpg'),
         description: 'Iced Vietnamese coffee with condensed milk'
     },
     {
         id: 15,
-        name: 'Cà Phê Đen Đá',
+        name: 'Cà Phê Đen - (Black coffee)',
         type: 'drinks',
         image: require('../assets/images/ca-phe-den.png'),
         description: 'Iced Vietnamese coffee black'
     },
     {
         id: 16,
-        name: 'Cà phê trứng',
+        name: 'Cà phê trứng - (Egg coffee)',
         type: 'drinks',
         image: require('../assets/images/ca-phe-trung.jpg'),
         description: 'Hot Vietamese egg coffee with egg yolks, sugar, and condensed milk.'
     },
     {
         id: 17,
-        name: 'Dừa tắc',
+        name: 'Dừa tắc - (Iced tea)',
         type: 'drinks',
         image: require('../assets/images/dua-tac.jpg'),
         description: 'Iced green tea with coconut and kumquat juice'
