b7ed2d8f4959d37b0282faeebae8550401749dd7
jacobnathanson <jacobnath93@gmail.com>
2022-02-02 12:54:56 -0500

    main stylesheet

--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
 import HomeScreen from './Home';
 import MenuScreen from './Menu';
 import ReviewOrderScreen from './ReviewOrder';
 import CheckoutScreen from './CheckoutPage';
 import MenuItemScreen from './MenuItem';
-import { color } from 'react-native-elements/dist/helpers';
 
 const Stack = createStackNavigator();
 
@@ -14,16 +14,7 @@ export default function Main() {
         <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
             <Stack.Screen name='Menu' component={MenuScreen} options={{
-                headerStyle: {
-                    alignItems: 'center',
-
-                },
-                headerTitleStyle: {
-                    color: '#3e5d18',
-                    fontFamily: 'DMSans_400Regular',
-                    fontSize: 30
-
-                },
+                headerTitleStyle: style.headerTitle,
                 headerTitleAlign: 'center'
             }} />
             <Stack.Screen name='MenuItem' component={MenuItemScreen} options={{ presentation: 'modal', }} />
@@ -32,6 +23,15 @@ export default function Main() {
         </Stack.Navigator>
     )
 }
+
+const style = new StyleSheet.create({
+    headerTitle: {
+        color: '#3e5d18',
+        fontFamily: 'DMSans_400Regular',
+        fontSize: 30
+    }
+})
+
 
 
 
