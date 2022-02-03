95cf9701cc54af0b9a8441f4bee44a9fde8476a1
jacobnathanson <jacobnath93@gmail.com>
2022-02-03 17:23:57 -0500

    screen stack title

--- components/Main.js
+++ components/Main.js
@@ -13,12 +13,18 @@ export default function Main() {
     return (
         <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
-            <Stack.Screen name='Menu' component={MenuScreen} options={{
-                headerTitleStyle: styles.headerTitle,
-                headerTitleAlign: 'center'
-            }} />
+            <Stack.Screen name='Menu' component={MenuScreen}
+                options={{
+                    headerTitleStyle: styles.headerTitle,
+                    headerTitleAlign: 'center'
+                }} />
             <Stack.Screen name='MenuItem' component={MenuItemScreen} options={{ presentation: 'modal', headerShown: false }} />
-            <Stack.Screen name='ReviewOrder' component={ReviewOrderScreen} />
+            <Stack.Screen name='ReviewOrder' component={ReviewOrderScreen}
+                options={{
+                    title: 'Review',
+                    headerTitleStyle: styles.headerTitle,
+                    headerTitleAlign: 'center'
+                }} />
             <Stack.Screen name='Checkout' component={CheckoutScreen} />
         </Stack.Navigator>
     )
