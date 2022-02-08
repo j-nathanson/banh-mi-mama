57763b707ca1161d9afc5bc56af8236780e97561
jacobnathanson <jacobnath93@gmail.com>
2022-02-08 17:03:49 -0500

    add scrollview to home input form

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import { Button, Overlay, Input, Icon } from 'react-native-elements'
 import { LinearGradient } from 'expo-linear-gradient';
 import { useSelector, useDispatch } from "react-redux";
@@ -91,85 +91,87 @@ export default function HomeScreen({ navigation }) {
                 </View>
             </LinearGradient >
             <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={{ width: '90%', }} animationType='slide' >
-                <Text style={{ fontSize: 25, fontFamily: 'DMSans_400Regular', marginBottom: 10, textAlign: 'center' }}>Enter Your Info!</Text>
+                <ScrollView>
+                    <Text style={{ fontSize: 25, fontFamily: 'DMSans_400Regular', marginBottom: 10, textAlign: 'center' }}>Enter Your Info!</Text>
 
-                <Input
-                    label='First Name'
-                    placeholder='Sean'
-                    leftIcon={{ type: 'font-awesome', name: 'user-o' }}
-                    onChangeText={(value) => dispatch(updateUserProperty({ name: 'firstName', value: value }))}
-                    value={firstName}
+                    <Input
+                        label='First Name'
+                        placeholder='Sean'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={(value) => dispatch(updateUserProperty({ name: 'firstName', value: value }))}
+                        value={firstName}
 
-                />
-                <Input
-                    label='Last Name'
-                    placeholder='Nguyen'
-                    leftIcon={{ type: 'entypo', name: 'man' }}
-                    onChangeText={(value) => dispatch(updateUserProperty({ name: 'lastName', value: value }))}
-                    value={lastName}
-                />
-
-                <Input
-                    placeholder='xxx-xxx-xxxx'
-                    label='Phone Number'
-                    leftIcon={{ type: 'antdesign', name: 'phone' }}
-                    onChangeText={(value) => dispatch(updateUserProperty({ name: 'phone', value: value }))}
-                    value={phone}
-                />
-                <Input
-                    label='Email'
-                    placeholder='CaPhe365@gmail.com'
-                    leftIcon={{ type: 'feather', name: 'mail' }}
-                    onChangeText={(value) => dispatch(updateUserProperty({ name: 'email', value: value }))}
-                    value={email}
-                />
-
-                {orderType === 'delivery' ?
-                    <>
-                        <Input
-                            placeholder='33 North Front St'
-                            label='Street address'
-                            leftIcon={{ type: 'font-awesome-5', name: 'house-user' }}
-                            onChangeText={(value) => dispatch(updateUserProperty({ name: 'address', value: value }))}
-                            value={address}
-
-                        />
-                        <Input
-                            label='Apartment Number'
-                            placeholder='6B'
-                            leftIcon={{ type: 'material-community', name: 'doorbell' }}
-                            onChangeText={(value) => dispatch(updateUserProperty({ name: 'aptNum', value: value }))}
-                            value={aptNum}
-                        />
-                    </>
-                    : <View />
-                }
-                <View style={{ alignItems: 'center' }}>
-                    <Button
-                        title="Start Your Order"
-                        icon={{
-                            name: 'shopping-bag',
-                            type: 'entypo',
-                            size: 15,
-                            color: 'white',
-                        }}
-                        iconContainerStyle={{ marginRight: 10 }}
-                        titleStyle={{ fontWeight: '700' }}
-                        buttonStyle={{
-                            backgroundColor: '#323232',
-                            borderColor: 'transparent',
-                            borderRadius: 30,
-                        }}
-                        containerStyle={{
-                            width: 200,
-
-                        }}
-                        onPress={() => {
-                            toggleOverlay()
-                            navigation.navigate('Menu')
-                        }}
                     />
-                </View>
+                    <Input
+                        label='Last Name'
+                        placeholder='Nguyen'
+                        leftIcon={{ type: 'entypo', name: 'man' }}
+                        onChangeText={(value) => dispatch(updateUserProperty({ name: 'lastName', value: value }))}
+                        value={lastName}
+                    />
+
+                    <Input
+                        placeholder='xxx-xxx-xxxx'
+                        label='Phone Number'
+                        leftIcon={{ type: 'antdesign', name: 'phone' }}
+                        onChangeText={(value) => dispatch(updateUserProperty({ name: 'phone', value: value }))}
+                        value={phone}
+                    />
+                    <Input
+                        label='Email'
+                        placeholder='CaPhe365@gmail.com'
+                        leftIcon={{ type: 'feather', name: 'mail' }}
+                        onChangeText={(value) => dispatch(updateUserProperty({ name: 'email', value: value }))}
+                        value={email}
+                    />
+
+                    {orderType === 'delivery' ?
+                        <>
+                            <Input
+                                placeholder='33 North Front St'
+                                label='Street address'
+                                leftIcon={{ type: 'font-awesome-5', name: 'house-user' }}
+                                onChangeText={(value) => dispatch(updateUserProperty({ name: 'address', value: value }))}
+                                value={address}
+
+                            />
+                            <Input
+                                label='Apartment Number'
+                                placeholder='6B'
+                                leftIcon={{ type: 'material-community', name: 'doorbell' }}
+                                onChangeText={(value) => dispatch(updateUserProperty({ name: 'aptNum', value: value }))}
+                                value={aptNum}
+                            />
+                        </>
+                        : <View />
+                    }
+                    <View style={{ alignItems: 'center' }}>
+                        <Button
+                            title="Start Your Order"
+                            icon={{
+                                name: 'shopping-bag',
+                                type: 'entypo',
+                                size: 15,
+                                color: 'white',
+                            }}
+                            iconContainerStyle={{ marginRight: 10 }}
+                            titleStyle={{ fontWeight: '700' }}
+                            buttonStyle={{
+                                backgroundColor: '#323232',
+                                borderColor: 'transparent',
+                                borderRadius: 30,
+                            }}
+                            containerStyle={{
+                                width: 200,
+
+                            }}
+                            onPress={() => {
+                                toggleOverlay()
+                                navigation.navigate('Menu')
+                            }}
+                        />
+                    </View>
+                </ScrollView>
             </Overlay>
         </View >
     );
